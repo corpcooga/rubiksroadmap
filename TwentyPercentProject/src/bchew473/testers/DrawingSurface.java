7fae95437bfa52ee5612528bb6cb0aff70d1f431
Boon <bchew473@student.fuhsd.org>
2023-10-10 18:06:53 -0700

    DrawingSurface shows

--- TwentyPercentProject/src/bchew473/testers/DrawingSurface.java
+++ TwentyPercentProject/src/bchew473/testers/DrawingSurface.java
@@ -1,6 +1,27 @@
 package bchew473.testers;
 
-public class DrawingSurface 
+import processing.core.PApplet;
+
+public class DrawingSurface extends PApplet
 {
+	public DrawingSurface()
+	{
+		
+	}
+	
+	public void settings()
+	{
+		setSize(1000, 800);
+	}
+	
+	public void setup()
+	{
+		
+	}
+	
+	public void draw()
+	{
+		
+	}
 
 }
