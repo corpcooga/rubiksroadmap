5c7f987896228c3a46fcf5e76a4dda98591e28a0
Boon <bchew473@student.fuhsd.org>
2023-10-16 09:04:40 -0700

    Button class + constructors

---
+++ TwentyPercentProject/src/bchew473/components/Button.java
@@ -0,0 +1,48 @@
+package bchew473.components;
+
+import processing.core.PApplet;
+import java.awt.Color;
+
+public class Button {
+	
+//	Fields
+	
+	double x, y, width, height;
+	int borderWidth;
+	Color fillColor, borderColor;
+	
+	
+//	Constructors
+	
+	public Button(double x, double y, double width, double height, int bWidth, Color fCol, Color bCol)
+	{
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		borderWidth = bWidth;
+		fillColor = fCol;
+		borderColor = bCol;
+	}
+	
+	public Button(double x, double y, double width, double height, int bWidth)
+	{
+		this(x, y, width, height, bWidth, Color.white, Color.black);
+	}
+	
+	public Button(double x, double y, double width, double height)
+	{
+		this(x, y, width, height, 1);
+	}
+	
+	public Button()
+	{
+		this(0, 0, 0, 0);
+	}
+	
+	
+//	Methods
+	
+	
+	
+}
