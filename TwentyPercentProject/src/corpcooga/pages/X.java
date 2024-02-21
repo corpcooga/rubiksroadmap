7de4c89a2506ece0b3a748bb353121bfe31302a8
Boon <bchew473@student.fuhsd.org>
2024-02-20 16:16:39 -0800

    added non-title page images

--- TwentyPercentProject/src/corpcooga/pages/X.java
+++ TwentyPercentProject/src/corpcooga/pages/X.java
@@ -29,7 +29,7 @@ public class X
 	
 	public void displayPage(PApplet p)
 	{
-		p.push();
+//		p.push();
 		
 //		p.textAlign(PApplet.CENTER);
 //		p.imageMode(PApplet.CENTER);
@@ -55,9 +55,9 @@ public class X
 //			p.textAlign(PApplet.CENTER);
 //			p.textSize(36);
 //			p.text("Side", DrawingSurface.DRAWING_WIDTH / 2 - 200, 220);
-			p.image(p.loadImage("resources/img/solved_side.png"), DrawingSurface.DRAWING_WIDTH / 2 - 200, 320, 180, 180);
+//			p.image(p.loadImage("resources/img/solved_side.png"), DrawingSurface.DRAWING_WIDTH / 2 - 200, 320, 180, 180);
 //			p.text("Layer", DrawingSurface.DRAWING_WIDTH / 2 + 200, 220);
-			p.image(p.loadImage("resources/img/solved_layer.png"), DrawingSurface.DRAWING_WIDTH / 2 + 200, 320, 180, 180);
+//			p.image(p.loadImage("resources/img/solved_layer.png"), DrawingSurface.DRAWING_WIDTH / 2 + 200, 320, 180, 180);
 //			p.pop();
 			
 //			p.text(text.get(0)[3], 100, 430, DrawingSurface.DRAWING_WIDTH - 200, DrawingSurface.DRAWING_HEIGHT - 100);
@@ -66,11 +66,11 @@ public class X
 //			p.textAlign(PApplet.CENTER);
 //			p.textSize(30);
 //			p.text("Center pieces", DrawingSurface.DRAWING_WIDTH / 2 - 300, 620);
-			p.image(p.loadImage("resources/img/center_pieces.png"), DrawingSurface.DRAWING_WIDTH / 2 - 300, 680, 100, 100);
+//			p.image(p.loadImage("resources/img/center_pieces.png"), DrawingSurface.DRAWING_WIDTH / 2 - 300, 680, 100, 100);
 //			p.text("Edge pieces", DrawingSurface.DRAWING_WIDTH / 2, 620);
-			p.image(p.loadImage("resources/img/edge_pieces.png"), DrawingSurface.DRAWING_WIDTH / 2, 680, 100, 100);
+//			p.image(p.loadImage("resources/img/edge_pieces.png"), DrawingSurface.DRAWING_WIDTH / 2, 680, 100, 100);
 //			p.text("Corner pieces", DrawingSurface.DRAWING_WIDTH / 2 + 300, 620);
-			p.image(p.loadImage("resources/img/corner_pieces.png"), DrawingSurface.DRAWING_WIDTH / 2 + 300, 680, 100, 100);
+//			p.image(p.loadImage("resources/img/corner_pieces.png"), DrawingSurface.DRAWING_WIDTH / 2 + 300, 680, 100, 100);
 //			p.pop();
 			
 			break;
@@ -81,17 +81,17 @@ public class X
 //			p.push();
 //			p.textAlign(PApplet.CENTER);
 //			p.textSize(36);
-			p.image(p.loadImage("resources/img/front_side.png"), DrawingSurface.DRAWING_WIDTH / 2 - 500, 270, 100, 100);
+//			p.image(p.loadImage("resources/img/front_side.png"), DrawingSurface.DRAWING_WIDTH / 2 - 500, 270, 100, 100);
 //			p.text("F", DrawingSurface.DRAWING_WIDTH / 2 - 500, 355);
-			p.image(p.loadImage("resources/img/back_side.png"), DrawingSurface.DRAWING_WIDTH / 2 - 300, 270, 100, 100);
+//			p.image(p.loadImage("resources/img/back_side.png"), DrawingSurface.DRAWING_WIDTH / 2 - 300, 270, 100, 100);
 //			p.text("B", DrawingSurface.DRAWING_WIDTH / 2 - 300, 355);
-			p.image(p.loadImage("resources/img/up_side.png"), DrawingSurface.DRAWING_WIDTH / 2 - 100, 270, 100, 100);
+//			p.image(p.loadImage("resources/img/up_side.png"), DrawingSurface.DRAWING_WIDTH / 2 - 100, 270, 100, 100);
 //			p.text("U", DrawingSurface.DRAWING_WIDTH / 2 - 100, 355);
-			p.image(p.loadImage("resources/img/down_side.png"), DrawingSurface.DRAWING_WIDTH / 2 + 100, 270, 100, 100);
+//			p.image(p.loadImage("resources/img/down_side.png"), DrawingSurface.DRAWING_WIDTH / 2 + 100, 270, 100, 100);
 //			p.text("D", DrawingSurface.DRAWING_WIDTH / 2 + 100, 355);
-			p.image(p.loadImage("resources/img/left_side.png"), DrawingSurface.DRAWING_WIDTH / 2 + 300, 270, 100, 100);
+//			p.image(p.loadImage("resources/img/left_side.png"), DrawingSurface.DRAWING_WIDTH / 2 + 300, 270, 100, 100);
 //			p.text("L", DrawingSurface.DRAWING_WIDTH / 2 + 300, 355);
-			p.image(p.loadImage("resources/img/right_side.png"), DrawingSurface.DRAWING_WIDTH / 2 + 500, 270, 100, 100);
+//			p.image(p.loadImage("resources/img/right_side.png"), DrawingSurface.DRAWING_WIDTH / 2 + 500, 270, 100, 100);
 //			p.text("R", DrawingSurface.DRAWING_WIDTH / 2 + 500, 355);
 //			p.pop();
 			
@@ -104,10 +104,10 @@ public class X
 			
 		case 5:
 //			p.text(text.get(1)[0], 100, 120, DrawingSurface.DRAWING_WIDTH - 400, DrawingSurface.DRAWING_HEIGHT - 100);
-			p.image(p.loadImage("resources/img/the_cross.png"), DrawingSurface.DRAWING_WIDTH - 200, 200, 200, 200);
+//			p.image(p.loadImage("resources/img/the_cross.png"), DrawingSurface.DRAWING_WIDTH - 200, 200, 200, 200);
 			
 //			p.text(text.get(1)[1], 300, 316, DrawingSurface.DRAWING_WIDTH - 400, DrawingSurface.DRAWING_HEIGHT - 100);
-			p.image(p.loadImage("resources/img/the_daisy.png"), 180, 410, 200, 200);
+//			p.image(p.loadImage("resources/img/the_daisy.png"), 180, 410, 200, 200);
 			
 //			p.text(text.get(1)[2], 100, 540, DrawingSurface.DRAWING_WIDTH - 200, DrawingSurface.DRAWING_HEIGHT - 100);
 			break;
@@ -119,16 +119,16 @@ public class X
 		case 7:
 //			p.text(text.get(2)[0], 100, 120, DrawingSurface.DRAWING_WIDTH - 200, DrawingSurface.DRAWING_HEIGHT - 100);
 //			p.text(text.get(2)[1], 100, 230, DrawingSurface.DRAWING_WIDTH - 350, DrawingSurface.DRAWING_HEIGHT - 100);
-			p.image(p.loadImage("resources/img/example_corner_piece.png"), DrawingSurface.DRAWING_WIDTH - 160, 290, 140, 140);
-			p.image(p.loadImage("resources/img/first_layer_corner_up.png"), 160, 450, 140, 140);
+//			p.image(p.loadImage("resources/img/example_corner_piece.png"), DrawingSurface.DRAWING_WIDTH - 160, 290, 140, 140);
+//			p.image(p.loadImage("resources/img/first_layer_corner_up.png"), 160, 450, 140, 140);
 //			p.text(text.get(2)[2], 250, 380, DrawingSurface.DRAWING_WIDTH - 500, DrawingSurface.DRAWING_HEIGHT - 100);
-			p.image(p.loadImage("resources/img/first_layer_corner_down.png"), DrawingSurface.DRAWING_WIDTH - 160, 450, 140, 140);
+//			p.image(p.loadImage("resources/img/first_layer_corner_down.png"), DrawingSurface.DRAWING_WIDTH - 160, 450, 140, 140);
 			break;
 			
 		default:
 			p.text("Invalid Page", DrawingSurface.DRAWING_WIDTH / 2, DrawingSurface.DRAWING_HEIGHT / 2);
 		}
-		p.pop();
+//		p.pop();
 	}
 	
 }
